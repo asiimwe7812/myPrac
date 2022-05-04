b118966a207c7b681f0c50b12ec774582c263039
asiimwe7812 <asiimwe7610@gmail.com>
2022-05-04 11:33:19 +0300

    I add loading and messagebox

---
+++ shopy/src/components/MessagingBox.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function MessagingBox() {
+  return (
+    <div>
+      <i className="fa fa-spinner fa-spin">Loading...</i>
+    </div>
+  );
+}
--- shopy/src/screens/HomeScreen.js
+++ shopy/src/screens/HomeScreen.js
@@ -1,22 +1,38 @@
 import React, { useEffect, useState } from "react";
 import Product from "../components/Product.js";
 import axios from "axios";
+import LoadingBox from "../components/Loadingbox.js";
 function HomeScreen() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
   return (
     <div>
-      <div className="row center">
-        {products.map((product) => (
-          <Product key={product.slug} product={product}></Product>
-        ))}
-      </div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox>{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product.slug} product={product}></Product>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
